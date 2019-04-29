7c0d9dda3fb56bf76ed2fce1dcfa9f26d4fcb317
Phoenix-Miner <50107765+Phoenix-Miner@users.noreply.github.com>
2019-04-29 16:02:49 +0300

    Add files via upload

---
+++ 1_Ehereum-nanopool.bat
@@ -0,0 +1,2 @@
+PhoenixMiner.exe -pool eth-eu2.nanopool.org:9999 -wal 0x1a0e2c4cd699cee12672adc223fdb30b93253eba -worker Phoenix -epsw x -mode 1 -Rmode 1 -log 0 -mport 0 -etha 0 -retrydelay 1 -ftime 55 -tt 79 -tstop 89 -tstart 79 -fanmin 30 -coin eth
+pause
